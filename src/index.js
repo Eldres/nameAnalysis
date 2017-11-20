0e23fdf1b959d7d6082b55bdb6e325bdd1d3206b
Josh Nagel <nageljp27@gmail.com>
2017-11-19 18:05:19 -0800

    working beta version

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 var Alexa           = require('alexa-sdk');
 var alexaLib        = require('./alexaLib.js');
-var APP_ID 			= "amzn1.ask.skill.0f86fe6d-e239-468a-b876-a796628daed4";
+var APP_ID 			= "amzn1.ask.skill.ccbb2c53-f198-4f41-b158-0a5ac90e2557";
 var languageStrings = require('./languageStrings');
 var langEN          = languageStrings.en.translation;
 
@@ -15,40 +15,57 @@ exports.handler = function(event, context, callback) {
 };
 
 var handlers = {
-    'NameAnalysisIntent': function() {
-        this.emit('GetUserNameIntent');
-    },
-	'GetUserNameIntent': function() {
-        var requestedName   = alexaLib.validateAndSetSlot(this.event.request.intent.slots.Name);
-        var myName          = langEN.NAMES[requestedName];
+	'NameAnalysisIntent': function() {
+        var requestedName     = alexaLib.validateAndSetSlot(this.event.request.intent.slots.Name);
+        var thisName          = langEN.NAMES[requestedName];
+        var thisNameDesc      = thisName.description;
         
         this.attributes['repromptSpeech'] = langEN.REPROMPT;
+        
+        /* working code
+        if(thisName){
+            this.attributes['name'] = requestedName; //store user input into session attribute, if given name is in dict
+            thisName = this.attributes['name'];
+        }
+        this.attributes['speechOutput'] = "Hi, " + thisName + ". Whats your gender?";
+        this.emit(':ask', this.attributes['speechOutput']);
+        */
 
-        if (myName){ //get user name
-            this.attributes['name'] = myName;
-        }else if(requestedName){ //if user provides a name that is not in my dictionary
-            this.attributes['speechOutput'] = alexaLib.notFoundMessage(this.event.request.intent.slots.Name.name, requestedName);
+        if(thisName){
+            this.attributes['name'] = requestedName; //store user input into session attribute, if given name is in dict
+            thisName = this.attributes['name'];
+            this.attributes['description'] = thisNameDesc;
+        }
+        else if(requestedName){ //if user provides a name that is not in my dictionary
+            this.attributes['speechOutput'] = alexaLib.notFoundMessage(this.event.request.intent.slots.Name.name, requestedName); //returns not found message if user provides name not listed in dict
         }else{
             this.attributes['speechOutput'] = langEN.UNHANDLED;
         }
 
-        this.attributes['speechOutput'] = "Hi, " + this.attributes['name'] + ". Whats your gender?";
-        this.emit(':ask', this.attributes['speechOutput'], 'GetUserGenderIntent');
+        this.attributes['speechOutput'] = "Hi, " + thisName + ". Whats your gender?";
+        this.emit(':ask', this.attributes['speechOutput']);
     },
     'GetUserGenderIntent': function() {
         var requestedGender  = alexaLib.validateAndSetSlot(this.event.request.intent.slots.Gender);
         var myGender         = langEN.GENDERS[requestedGender];
-        var UserName         = this.attributes['name'];
+        var myName           = this.attributes['name'];
+        var myNameDesc       = this.attributes['description'];
         
         this.attributes['repromptSpeech'] = langEN.REPROMPT;
 
-        if(myGender){ //get user gender
-            this.emit(':ask', UserName);
-        }else if(requestedName){ //if user provides a gender that is not in my dictionary
+        if(myGender){ //checks to see if the provided gender exists in the dictionary
+            this.attributes['speechOutput'] = myNameDesc;
+            // if(myGender === "male"){ //this logic checks out -- if(myGender === "male"){
+            //     this.attributes['speechOutput'] = myNameDesc;
+            // }
+        }
+        else if(requestedName){ //if user provides a gender that is not in my dictionary
             this.attributes['speechOutput'] = alexaLib.notFoundMessage(this.event.request.intent.slots.Gender.gender, requestedGender);
         }else{
             this.attributes['speechOutput'] = langEN.UNHANDLED;
         }
+
+        this.emit(':tell', this.attributes['speechOutput']);
     },
 	'Unhandled': function () {
         this.attributes['continue']         = true;
@@ -60,7 +77,10 @@ var handlers = {
         // Alexa, ask [my-skill-invocation-name] to (do something)...
         // If the user either does not reply to the welcome message or says something that is not
         // understood, they will be prompted again with this text.
-        this.emit(':ask', 'Welcome to Name Analysis. I can help you understand whether your first name is helping or hurting you… Please tell me your first name…');
+        this.attributes['continue']         = true;
+        this.attributes['speechOutput']     = langEN.WELCOME_MESSAGE;
+        this.attributes['repromptSpeech']   = langEN.WELCOME_REPROMPT;
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech']);
     },
     'AMAZON.HelpIntent': function () {
         this.attributes['speechOutput'] = langEN.HELP_MESSAGE;
--- src/languageStrings.js
+++ src/languageStrings.js
@@ -4,8 +4,9 @@ var genders	= require('./genders');
 module.exports = {
 	"en": {
 		"translation": {
-			"NAMES" 	: names.NAMES,
-			"GENDERS" 	: genders.GENDERS,
+			"NAMES" 			: names.NAMES,
+			"NAMES_ATTRIBUTES"  : names.NAMES_ATTRIBUTES,
+			"GENDERS" 			: genders.GENDERS,
 
 			"CONTINUE_PROMPT" 	: " Now, what was your question?",
 			"NOT_FOUND_MESSAGE" : "I'm sorry, I currently do not know ",
@@ -13,7 +14,7 @@ module.exports = {
 			"REPROMPT" 			: "Would you like analyize a different name?",
 			"STOP_MESSAGE" 		: "Goodbye!",
 			"UNHANDLED" 		: "I'm sorry, I didn't get that. You can repeat that command.",
-			"WELCOME_MESSAGE" 	: "Welcome to Name Analysis. You can ask questions to learn about the meaning behind someones name.",
+			"WELCOME_MESSAGE" 	: "Welcome to Name Analysis. You can ask questions to learn about the meaning behind someones name. Please tell me your first name...",
 			"WELCOME_REPROMPT" 	: "For instructions on what you can say, please say help me."
 		}
 	}
